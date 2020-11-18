d6d2e7e09df813cd5aa59c0553b416e6aaa2dba3
Tim <timknapen@gmail.com>
2020-11-18 17:12:14 +0100

    initial commit

---
+++ slideshow.py
@@ -0,0 +1,121 @@
+'''
+Python 2.7 slideshow for Glenn
+modified from https://gist.github.com/terencewu/034e09f0e318c621516b
+
+All images must be dropped in a folder called ./images
+
+Usage: python slideShow.py
+'''
+
+import Tkinter as tk
+from PIL import Image, ImageTk
+import time
+import sys
+import os
+import random
+
+
+
+class HiddenRoot(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        #hackish way, essentially makes root window
+        #as small as possible but still "focused"
+        #enabling us to use the binding on <esc>
+        self.wm_geometry("0x0+0+0")
+
+        self.window = MySlideShow(self)
+        self.window.startSlideShow()
+
+
+class MySlideShow(tk.Toplevel):
+    def __init__(self, *args, **kwargs):
+        tk.Toplevel.__init__(self, *args, **kwargs)
+      
+        #remove window decorations 
+        self.overrideredirect(True)
+
+        #save reference to photo so that garbage collection
+        #does not clear image variable in show_image()
+        self.persistent_image = None
+        self.imageList = []
+        self.pixNum = 0
+
+        #used to display as background image
+        self.label = tk.Label(self)
+        self.label.pack(side="top", fill="both", expand=True)
+        self.label.configure(background='black')
+
+        self.getImages()
+        
+        
+
+    def getImages(self):
+        '''
+        Get image directory from command line or use current directory
+        '''
+        print("getting images...")
+    
+        curr_dir = './images'
+
+        for root, dirs, files in os.walk(curr_dir):
+            for f in files:
+                if f.endswith(".png") or f.endswith(".jpg"):
+                    img_path = os.path.join(root, f)
+                    print(img_path)
+                    self.imageList.append(img_path)
+
+    def startSlideShow(self, delay=1): #delay in seconds
+        myimage = random.choice(self.imageList) 
+        # self.imageList[self.pixNum]
+        # self.pixNum = (self.pixNum + 1) % len(self.imageList)
+        self.showImage(myimage)
+        #its like a callback function after n seconds (cycle through pics)
+        self.after(delay*1000, self.startSlideShow)
+
+    def showImage(self, filename):
+        print "Showing image: " , filename
+        image = Image.open(filename)  
+
+        img_w, img_h = image.size
+        scr_w, scr_h = self.winfo_screenwidth(), self.winfo_screenheight()
+
+        imgScale = max(float(scr_w)/float(img_w), float(scr_h)/float(img_h))
+        print "  image scale: " , imgScale
+
+        width, height = min(scr_w, img_w), min(scr_h, img_h)
+        if imgScale > 0:
+            width, height = img_w * imgScale, img_h * imgScale
+        else:
+            print " WARNING imgscale " , imgScale
+        print "  screen size: " , scr_w , " x " , scr_h, "  image size: " , img_w , " x " , img_h
+        print "  scaled: " , width , " x ", height
+
+        image.thumbnail((width, height), Image.ANTIALIAS) #resizes image, makes everything faster
+
+        #set window size after scaling the original image up/down to fit screen
+        #removes the border on the image
+        # scaled_w, scaled_h = image.size
+        # self.wm_geometry("{}x{}+{}+{}".format(scaled_w,scaled_h,0,0))
+        self.wm_geometry("{}x{}+{}+{}".format(scr_w,scr_h,0,0))
+
+        # create new image 
+        self.persistent_image = ImageTk.PhotoImage(image)
+        self.label.configure(image=self.persistent_image)
+
+
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
+
+'''
+#create main window
+master = tk.Tk()
+master.title("tester")
+master.geometry("300x100")
+'''
+
+
+slideShow = HiddenRoot()
+slideShow.bind("<Escape>", lambda e: slideShow.destroy())  # exit on esc
+slideShow.mainloop()
